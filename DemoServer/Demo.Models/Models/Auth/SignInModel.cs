48a33fd07546ff74bdf6ebddec6052b64854eb12
Anatoliy Ignatev <anatoliyignatev@gmail.com>
2021-06-04 20:53:53 +0300

    Add pasword hasher

---
+++ DemoServer/Demo.Models/Models/Auth/SignInModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Demo.Models.Models.Auth
+{
+    public class SignInModel
+    {
+        public string Email { get; set; }
+        
+        public string Password { get; set; }
+    }
+}
--- DemoServer/Demo.WebApi/Controllers/Auth/AuthController.cs
+++ DemoServer/Demo.WebApi/Controllers/Auth/AuthController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Demo.WebApi.Common.DbConnection;
 using Demo.WebApi.Common.Enums;
 using Demo.WebApi.Common.ResultPackage;
+using Demo.WebApi.Common.Security;
 
 namespace Demo.WebApi.Controllers.Auth
 {
@@ -34,7 +35,7 @@ namespace Demo.WebApi.Controllers.Auth
                             birth_date AS {nameof(UserModel.BirthDate)},
                             register_date AS {nameof(UserModel.RegisterDate)}
                       FROM public.users 
-                      WHERE user_id = @userId";
+                      WHERE user_id = @userId;";
 
                 using (_connection)
                 {
@@ -64,16 +65,17 @@ namespace Demo.WebApi.Controllers.Auth
             }
         }
         
-        [HttpPost("RegisterUser")]
-        public Result<UserRegistrationModel> GetUserInfoById(UserRegistrationModel user)
+        [HttpPost("SignUp")]
+        public Result<bool> SignUp(UserRegistrationModel user)
         {
             var validator = new UserRegistrationValidator();
             var result = validator.Validate(user);
 
             if (!result.IsValid)
             {
-                return new Result<UserRegistrationModel>
+                return new Result<bool>
                 {
+                    Data = false,
                     Message = result.Errors.ToString(),
                     ResultCode = (int) ResultCode.Error
                 };
@@ -81,17 +83,47 @@ namespace Demo.WebApi.Controllers.Auth
             
             try
             {
-                var sql = 
-                    @"INSERT INTO public.users
-                      (first_name, last_name, birth_date, email, password_hash, register_date)
-	                  VALUES (@FIRSTNAME, ?, ?, ?, ?, ?);";
-
                 using (_connection)
                 {
-                    var count = _connection.Execute(sql);
+                    _connection.Open();
+                    
+                    var sql = 
+                        @"SELECT COUNT(*)
+                          FROM public.users 
+                          WHERE email = @Email;";
+                    
+                    var dublicateCount = _connection.ExecuteScalar<long>(sql, new { user.Email });
 
-                    return new Result<UserRegistrationModel>
+                    if (dublicateCount != 0)
                     {
+                        return new Result<bool>
+                        {
+                            Data = false,
+                            Message = "This email already exist.",
+                            ResultCode = (int) ResultCode.Success
+                        };
+                    }
+                    
+                    sql = 
+                        @"INSERT INTO public.users
+                       (first_name, last_name, birth_date, email, password_hash, register_date)
+	                   VALUES (@FirstName, @LastName, @BirthDate, @Email, @passwordHash, NOW());";
+                    
+                    var passwordHash = PasswordHasher.Hash(user.Password);
+                    
+                    _connection.Execute(sql,
+                        new
+                        {
+                            user.FirstName,
+                            user.LastName,
+                            user.BirthDate,
+                            user.Email,
+                            passwordHash
+                        });
+
+                    return new Result<bool>
+                    {
+                        Data = true,
                         Message = "Success",
                         ResultCode = (int) ResultCode.Success
                     };
@@ -100,8 +132,67 @@ namespace Demo.WebApi.Controllers.Auth
             }
             catch (Exception ex)
             {
-                return new Result<UserRegistrationModel>
+                return new Result<bool>
                 {
+                    Data = false,
+                    Message = ex.Message,
+                    ResultCode = (int) ResultCode.Error
+                };
+            }
+        }
+        
+        [HttpPost("SignIn")]
+        public Result<bool> SignIn(SignInModel model)
+        {
+            try
+            {
+                var sql = 
+                    @"SELECT 
+                             password_hash
+                      FROM public.users 
+                      WHERE email = @Email;";
+
+                using (_connection)
+                {
+                    var result = _connection.Query<string>(sql, new { model.Email }).ToList();
+
+                    if (!result.Any())
+                    {
+                        return new Result<bool>
+                        {
+                            Data = false,
+                            Message = "User don't sign up.",
+                            ResultCode = (int) ResultCode.Error
+                        };
+                    }
+                    
+                    var isCorrectPassword = PasswordHasher.Verify(model.Password, result.First());
+                    
+                    if (!isCorrectPassword)
+                    {
+                        return new Result<bool>
+                        {
+                            Data = false,
+                            Message = "Don't correct password.",
+                            ResultCode = (int) ResultCode.Error
+                        };
+                    }
+                    
+                    
+                    return new Result<bool>
+                    {
+                        Data = true,
+                        Message = "Success",
+                        ResultCode = (int) ResultCode.Success
+                    };
+                }
+               
+            }
+            catch (Exception ex)
+            {
+                return new Result<bool>
+                {
+                    Data = false,
                     Message = ex.Message,
                     ResultCode = (int) ResultCode.Error
                 };
